97ac435954f4b73d5dbad436b77272150a3d4466
natacha <nat_bartu@hotmail.com>
2022-11-03 12:09:59 +1000

    add icon from font awesome

--- src/components/tableField/TableField.js
+++ src/components/tableField/TableField.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const TableField = (fieldName) => {
-  return <div className="tableField">{fieldName}</div>;
+  return (
+    <div className="tableField">
+      {fieldName}
+      <i className="far fa-edit"></i>
+      <i className="far fa-trash-alt"></i>
+    </div>
+  );
 };
 
 export default TableField;
