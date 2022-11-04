6a3032a25607477ff44537cf1aecc3e03226da12
natacha <nat_bartu@hotmail.com>
2022-11-04 12:13:37 +1000

    add TextField Component from material ui inot Form Component

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FormControl, Typography, TextField } from "@mui/material";
 
 const Form = (props) => {
   const {
@@ -14,24 +15,34 @@ const Form = (props) => {
   } = props;
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Title</label>
-      <input
+    <FormControl onSubmit={handleSubmit}>
+      <Typography variant="h5" align="left" gutterBottom>
+        Title
+      </Typography>
+      <TextField
         autoFocus
         required
+        label="Required"
+        placeholder="Title"
         type="text"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
-      <label>Author</label>
-      <input
+      <Typography variant="h5" align="left" gutterBottom>
+        Author
+      </Typography>
+      <TextField
+        label="Required"
+        placeholder="Author"
         required
         type="text"
         value={author}
         onChange={(e) => setAuthor(e.target.value)}
       />
-      <label>ISBN#</label>
-      <input
+      <Typography variant="h5" align="left" gutterBottom>
+        ISBN#
+      </Typography>
+      <TextField
         required
         type="text"
         value={isbn}
@@ -41,7 +52,7 @@ const Form = (props) => {
         {currentBookId !== null ? "Update" : "Add"}
       </button>
       {currentBookId !== null && <button onClick={cancelEdit}>Cancel</button>}
-    </form>
+    </FormControl>
   );
 };
 
